a6a2a899da566070f23237cf7011d43f1f56cb82
Ismael <arkarash@gmail.com>
2020-07-14 12:46:54 +0200

    Suma de Strings

--- LletresRepetides/Program.cs
+++ LletresRepetides/Program.cs
@@ -6,13 +6,17 @@ namespace LletresRepetides
     {
         static void Main(string[] args)
         {
-            string Name = "Ismael Anaya Marquez";
-            char[] chars = Name.ToCharArray();
-            Console.WriteLine("Original string: {0}", Name);
-            Console.WriteLine("Character array:");
-            for (int ctr = 0; ctr < chars.Length; ctr++)
+            string Name = "Ismael";
+            string SurName = "Anaya";
+            char[] charName = Name.ToCharArray();
+            char[] charSurName = SurName.ToCharArray();
+
+            string FullName = Name + " " + SurName;
+            char[] charFullName = FullName.ToCharArray();
+
+            for (int ctr = 0; ctr < charFullName.Length; ctr++)
             {
-                Console.WriteLine("   {0}: {1}", ctr, chars[ctr]);
+                Console.WriteLine("   {0}: {1}", ctr, charFullName[ctr]);
             }
         }
     }
