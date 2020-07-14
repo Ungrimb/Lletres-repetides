3467259530fc2080abfc322b94552dfcbf096d56
Ismael <arkarash@gmail.com>
2020-07-14 09:37:48 +0200

    FASE1

---
+++ LletresRepetides/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LletresRepetides
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "Ismael Anaya Marquez";
+            char[] chars = Name.ToCharArray();
+            Console.WriteLine("Original string: {0}", Name);
+            Console.WriteLine("Character array:");
+            for (int ctr = 0; ctr < chars.Length; ctr++)
+            {
+                Console.WriteLine("   {0}: {1}", ctr, chars[ctr]);
+            }
+        }
+    }
+}
