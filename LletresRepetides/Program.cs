f76415570c75cc59c659a58c14f1b9ef3cf0231c
Ismael <arkarash@gmail.com>
2020-07-14 12:38:49 +0200

    Dictionary

--- LletresRepetides/Program.cs
+++ LletresRepetides/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LletresRepetides
 {
@@ -7,12 +9,25 @@ namespace LletresRepetides
         static void Main(string[] args)
         {
             string Name = "Ismael Anaya Marquez";
+            Name = Name.ToUpper();
             char[] chars = Name.ToCharArray();
+
+            Dictionary<char, int> charName = new Dictionary<char, int>();
             Console.WriteLine("Original string: {0}", Name);
-            Console.WriteLine("Character array:");
+            Console.WriteLine("Add to Dictionary:");
             for (int ctr = 0; ctr < chars.Length; ctr++)
             {
-                Console.WriteLine("   {0}: {1}", ctr, chars[ctr]);
+                int n = (from c in Name where c == chars[ctr] select c).Count();
+                //Console.WriteLine(n);
+                try
+                {
+                    charName.Add(chars[ctr], n);
+                }
+                catch (ArgumentException) { }
+            }
+            foreach (var pair in charName)
+            {
+                Console.WriteLine("FOREACH VAR: {0}, {1}", pair.Key, pair.Value);
             }
         }
     }
