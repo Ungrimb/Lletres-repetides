0e1463ae688017fdcececc0a38b8220db3bec9a3
Ismael <arkarash@gmail.com>
2020-07-14 10:48:34 +0200

    FASE 2

--- LletresRepetides/Program.cs
+++ LletresRepetides/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace LletresRepetides
 {
@@ -6,13 +9,23 @@ namespace LletresRepetides
     {
         static void Main(string[] args)
         {
-            string Name = "Ismael Anaya Marquez";
-            char[] chars = Name.ToCharArray();
+            string Name = "ismael ana8ya marquez";
+            char[] vocales = { 'a', 'e', 'i', 'o', 'u' };
+            int count = 0;
+            List<char> chars = new List<char>(Name);
             Console.WriteLine("Original string: {0}", Name);
             Console.WriteLine("Character array:");
-            for (int ctr = 0; ctr < chars.Length; ctr++)
+            foreach (char c in chars)
             {
-                Console.WriteLine("   {0}: {1}", ctr, chars[ctr]);
+                bool isVocal = false;
+                foreach (char v in vocales)
+                {
+                    if (v == c) isVocal = true;
+                }
+                if (char.IsNumber(c)) Console.WriteLine("   {0}: {1}  Els noms de persones no contenen números!", count, c);
+                if (isVocal) Console.WriteLine("   {0}: {1}  VOCAL", count, c);
+                else if (!char.IsNumber(c)) Console.WriteLine("   {0}: {1}", count, c);
+                count++;
             }
         }
     }
